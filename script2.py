a6e1e52f045a7f7b0bded4c7735eef54f6e984e4
Poornima223 <poornim@flemingcollege.ca>
2025-01-20 19:39:59 -0500

    added one more line to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # this is good programming advice
 print("In the face of ambiguity, refuse the temptation to guess.")
 print("Although that way may not be obvious at first unless you're Dutch")
+print("In the face of ambiguity, refuse the temptation to guess.")
