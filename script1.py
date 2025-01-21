d870cd22f644b2459221c2b235aa3b1f9195c52f
Poornima223 <poornim@flemingcollege.ca>
2025-01-20 19:31:37 -0500

    modified both script files
    -deleted print from script1.py
    -added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print lines of zen python
 print("Special cases aren't special enough to break the rules.")
-print("Unless explicitly silenced.")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# this is good programming advice
 print("In the face of ambiguity, refuse the temptation to guess.")
 print("Although that way may not be obvious at first unless you're Dutch")
